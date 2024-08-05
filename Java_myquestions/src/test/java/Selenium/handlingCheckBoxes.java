5d78b10b3e31848aa07739bc07f106d0525fac3b
AbdulSiyamwala <AbdulSiyamwala@FYNXTLAPIND018>
2024-08-05 10:47:32 +0530

    handlingCheckBoxes

---
+++ Java_myquestions/src/test/java/Selenium/handlingCheckBoxes.java
@@ -0,0 +1,46 @@
+package Selenium;
+
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class handlingCheckBoxes {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Initialize a new Chrome browser instance
+        WebDriver driver = new ChromeDriver();
+    
+        // Navigate to the specified URL
+            driver.get("https://testautomationpractice.blogspot.com/");
+
+        // Set an implicit wait timeout
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Maximize the browser window
+           driver.manage().window().maximize();
+
+        // Find all checkbox elements with the specified class and type
+           List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+
+         // Loop through the checkboxes and click those with odd indices
+            for (int i = 0; i < checkboxes.size(); i++) {
+         // Check if the index (0-based) is odd by using the modulo operation
+                if ((i + 1) % 2 == 1) {// Adding 1 to make it 1-based index
+                    WebElement checkbox = checkboxes.get(i);
+                    if (!checkbox.isSelected()) {
+                        checkbox.click();
+                    }
+                }
+            }
+
+            // Pause for a while to observe the results
+            Thread.sleep(5000);
+      
+            driver.quit();
+       
+    }
+}
