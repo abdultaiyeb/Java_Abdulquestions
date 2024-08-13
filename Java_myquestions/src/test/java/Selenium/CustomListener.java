746faef954ec9390b31e9401f697ec38985b82fc
AbdulSiyamwala <AbdulSiyamwala@FYNXTLAPIND018>
2024-08-13 17:28:50 +0530

    ITestListener

---
+++ Java_myquestions/src/test/java/Selenium/CustomListener.java
@@ -0,0 +1,42 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+
+public class CustomListener implements ITestListener  {
+	 WebDriver driver;
+
+	    @Override
+	    public void onTestStart(ITestResult result) {
+	        System.out.println("Test started: " + result.getName());
+	        
+	    }
+
+	    @Override
+	    public void onTestSuccess(ITestResult result) {
+	        System.out.println("Test passed: " + result.getName());
+	    }
+
+	    @Override
+	    public void onTestFailure(ITestResult result) {
+	        System.out.println("Test failed: " + result.getName());
+	        
+	    }
+
+	    @Override
+	    public void onTestSkipped(ITestResult result) {
+	        System.out.println("Test skipped: " + result.getName());
+	    }
+
+
+	    @Override
+	    public void onFinish(ITestContext context) {
+	        System.out.println("Test Suite finished: " + context.getName());
+	        
+	    }
+
+}
